d44cee08ef2b82cf1711188227c213425b2ecbe7
Retsuki <s1f101701845@iniad.org>
2020-04-17 21:04:56 +0900

    Add scraping_method.py and update scraping.py

--- src/scraping.py
+++ src/scraping.py
@@ -1,17 +1,12 @@
 import os
-import re
 import csv
 import glob
 import json
-import jaconv
 import requests
-import datetime
-import calendar
-import collections
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
+from scraping_method import *
 
 url = 'https://www.pref.ishikawa.lg.jp/kansen/coronakennai.html'
 res = requests.get(url)
@@ -23,85 +18,9 @@ h1_contents = tmp_contents.find_all('h1')
 all_contents_list = h1_contents[0].find_next_siblings()
 
 
-def data_shaping(date):
-    m = re.match(r'[0-9]+月[0-9]+日', date)
-    m_text = m.group()
-    pos = re.split('[月日]', m_text)
-    datetime_data = datetime.datetime(2020, int(pos[0]), int(pos[1]))
-    date = datetime_data.strftime("%Y-%m-%d")
-    return date
-
-
+# 陽性患者の属性を作成
 df = pd.DataFrame([], columns=['date', '居住地', '年代', '性別'])
-date = []
-residence = []
-age = []
-sex = []
-
-table = str.maketrans('（１）', '(1)')
-all_contents = ""
-now_date = ""
-now_age = ""
-new_text = ""
-new_text_list = []
-infect_count = 0
-for i in all_contents_list:
-    text = i.get_text().translate(table)
-    text2 = jaconv.z2h(text, kana=False, digit=True, ascii=True).replace(
-        " ", "").replace(":", "").replace("\xa0", "")
-
-    if "(1)年代" in text2:
-        m = re.search(r"\(1\)?年代(.+)", text2)
-        text_age = m.groups()[0]
-        age.append(text_age)
-    elif "(2)性別" in text2:
-        m = re.search(r"\(2\)性別(.+)", text2)
-        text_sex = (m.groups()[0])
-        sex.append(text_sex)
-    elif "(3)居住地" in text2:
-        m = re.search(r"\(3\)居住地(.+)", text2)
-        text_residence = (m.groups()[0])
-        residence.append(text_residence)
-
-    if "症状・経過" in text2:
-
-        # 新たなlistの要素を作る
-        new_text_list.append("empty")
-
-        # 初期設定
-        if now_age == "":
-            now_age = text2 + "check"
-            new_text += text2
-            infect_count += 1
-            continue
-
-        # now_ageとtext2の内容が違うのであれば
-        # (1)Save ever data
-        # (2)Delete ever data
-        # (3)Add new data
-        if now_age != text2:
-            new_text_list[infect_count-1] = new_text  # (1)
-            new_text = ""  # (2)
-            new_text += text2  # (3)
-            infect_count += 1
-            continue
-
-    if 0 < infect_count:
-        if "(5)行動歴" in text2:
-            continue
-        new_text += text2
-
-
-new_text_list[infect_count-1] = new_text
-
-for i in new_text_list:
-    target_index = i.find("陽性と判明")
-    result = i[:target_index]
-    s = re.findall(r'[0-9]+月[0-9]+日', result)
-    new_data = data_shaping(s[-1])
-    date.append(new_data)
-
-c = collections.Counter(date)
+date, residence, age, sex = create_pacients_table_DataFrame(all_contents_list)
 
 df['date'] = date
 df['居住地'] = residence
@@ -110,54 +29,38 @@ df['性別'] = sex
 
 patients_df = df.sort_values('date').reset_index(drop=True)
 patients_df.to_csv('./src/downloads/patients_data/patients.csv', index=False)
+patients_df_dict = patients_df.to_dict('index')
+data1 = [patients_df_dict.get(i) for i in range(len(patients_df_dict))]
 
-# 日付データの作成
+
+# 陽性患者数データを作成
 today = datetime.datetime.now()
 this_year = today.year
 this_month = today.month
 this_day = today.day
 this_hour = today.hour
 this_minute = today.minute
-today_info = datetime.datetime(this_year, this_month, 1)
-start_date = today_info.strftime("%Y-%m-%d")
-month_count = calendar.monthrange(this_year, this_month)[1]
-date_column = pd.date_range(start_date, freq='D', periods=this_day)
-subtotal_column = [0 for i in range(this_day)]
 
-# 空のデータフレームの作成
-progress_map = {'日付': date_column, '小計': subtotal_column}
-df = pd.DataFrame(progress_map)
+date_column, subtotal_column = create_patients_column(
+    this_year, this_month, this_day)
+x_month_data = create_x_month_data(date, date_column, subtotal_column)
+x_month_data.to_csv(
+    './src/downloads/each_data/{}_{}.csv'.format(this_year, this_month), index=False)
 
-# 条件にマッチした日にデータを挿入
-for num, i in enumerate(df.iloc[0:, 0]):
-    for j in c.keys():
-        if j in str(i):
-            df.iloc[num, 1] = c[j]
-
-# csv化
-df.to_csv('./src/downloads/each_data/{}_{}.csv'.format(this_year,
-                                                       this_month), index=False)
-
-# 各csvを連結
-csv_files = glob.glob('./src/downloads/each_data/*.csv')
+csv_files = glob.glob('../csv_data/each_data/*.csv')
 each_csv = []
 for i in csv_files:
     each_csv.append(pd.read_csv(i))
-df = pd.concat(each_csv).reset_index(drop=True)
+sum_x_month_data = pd.concat(each_csv).reset_index(drop=True)
 
-patients_summary_df = df.sort_values("日付").reset_index(drop=True)
+patients_summary_df = sum_x_month_data.sort_values("日付").reset_index(drop=True)
 patients_summary_df.to_csv("./src/downloads/final_data/total.csv", index=False)
-
-# patientsデータの作成
-patients_df_dict = patients_df.to_dict('index')
-data1 = [patients_df_dict.get(i) for i in range(len(patients_df_dict))]
-
-# patients_summaryデータの作成
 patients_summary_df_dict = patients_summary_df.to_dict('index')
 data2 = [patients_summary_df_dict.get(i)
          for i in range(len(patients_summary_df))]
 
-# 感染者と治療中の人数と退院数データの作成
+
+# 市区町村別の感染者データを作成
 table = soup.findAll("table")[0]
 tr = table.findAll("tr")
 with open('./src/downloads/table_data/corona_table.csv', "w", encoding="utf-8") as file:
@@ -165,18 +68,27 @@ with open('./src/downloads/table_data/corona_table.csv', "w", encoding="utf-8") 
     for i in tr:
         row = []
         for cell in i.findAll(["td", "th"]):
-            row.append(cell.get_text())
+            if cell.get_text() == '\u3000':
+                row.append('市区町村')
+            else:
+                row.append(cell.get_text())
         writer.writerow(row)
 
 table_df = pd.read_csv('./src/downloads/table_data/corona_table.csv')
-table_df.head()
+table_df = table_df.drop(20)
+table_df_dict = table_df.to_dict('index')
+data3 = [table_df_dict.get(i) for i in range(len(table_df))]
 
+
+# 感染者数, 入院者数, 死亡者数, 退院数
 final_row = table_df[-1:]
 total_infect = int(final_row["感染者"])
 treat = int(final_row["治療中"])
 death = int(final_row["死亡"])
 dischange = int(final_row["退院"])
 
+
+# jsonデータを作成
 update_at = "{}/{}/{} {}:{}".format(this_year,
                                     this_month, this_day, this_hour, this_minute)
 data_json = {
@@ -212,6 +124,10 @@ data_json = {
                 ]
             }
         ]
+    },
+    "city_summary": {
+        "lastUpdate": update_at,
+        "data": data3
     }
 }
 
